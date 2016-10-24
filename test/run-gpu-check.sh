b7c7a414b32b73e509e5b07c836ebbf5b1c6bccf
Taylor Lloyd <taylor@taylorlloyd.ca>
2016-10-24 17:08:15 -0600

    Functional GPUCheck capture script

--- test/run-gpu-check.sh
+++ test/run-gpu-check.sh
@@ -1,7 +1,14 @@
 #!/bin/bash
 CURDIR=`pwd`
 for f in $( ls profileData ); do
-  echo "Running analysis on $f"
+  cat profileData/$f/divergent | sort | uniq -u > profileData/$f/divergent.uniq
+  mv profileData/$f/divergent.uniq profileData/$f/divergent
+  cat profileData/$f/uncoalesced | sort | uniq -u > profileData/$f/uncoalesced.uniq
+  mv profileData/$f/uncoalesced.uniq profileData/$f/uncoalesced
+done
+echo "Benchmark, Div TP, Div FP, Div FN, Col TP, Col FP, Col FN"
+for f in $( ls profileData ); do
+  echo -n "$f"
   DD=${CURDIR}/gcheckData/$f
   mkdir -p $DD
   cd rodinia_3.1/cuda/$f
@@ -9,10 +16,29 @@ for f in $( ls profileData ); do
   make NVCC=${CURDIR}/gpu-check 2> $DD/tmp > $DD/tmp
   awk '/Divergence Issues:/{flag=1;next}/GPU-Check ran successfully/{flag=0}flag' $DD/tmp > $DD/divergent
   awk '/Memory Coalescing Issues:/{flag=1;next}/Divergence Issues:/{flag=0}flag' $DD/tmp > $DD/uncoalesced
-  rm $DD/tmp
   cd $CURDIR
-  echo "Cleaning Results"
   #Drop preceding ./
   sed -i 's/\.\///g' $DD/divergent
   sed -i 's/\.\///g' $DD/uncoalesced
+  #Sort and Unique results
+  cat $DD/divergent | sort | uniq -u > $DD/divergent.uniq
+  mv $DD/divergent.uniq $DD/divergent
+  cat $DD/uncoalesced | sort | uniq -u > $DD/uncoalesced.uniq
+  mv $DD/uncoalesced.uniq $DD/uncoalesced
+
+  #Print results
+  echo -n ", "
+  comm -1 -2 ${CURDIR}/profileData/$f/divergent $DD/divergent | wc -l | tr -d '\n'
+  echo -n ", "
+  diff ${CURDIR}/profileData/$f/divergent $DD/divergent | grep "^>" | wc -l | tr -d '\n'
+  echo -n ", "
+  diff ${CURDIR}/profileData/$f/divergent $DD/divergent | grep "^<" | wc -l | tr -d '\n'
+  echo -n ", "
+  comm -1 -2 ${CURDIR}/profileData/$f/uncoalesced $DD/uncoalesced | wc -l | tr -d '\n'
+  echo -n ", "
+  diff ${CURDIR}/profileData/$f/uncoalesced $DD/uncoalesced | grep "^>" | wc -l | tr -d '\n'
+  echo -n ", "
+  diff ${CURDIR}/profileData/$f/uncoalesced $DD/uncoalesced | grep "^<" | wc -l | tr -d '\n'
+  echo
+
 done
